137fff960d5dade5300274e0e33a219684eb64aa
nsmrcc00 <nsmrcc00@protonmail.com>
2024-04-27 23:15:37 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Login from './components/login/Login'
+import AdminHome from './components/admin/adminHome';
+import { AuthProvider } from './contexts/authContexts';
+import { useRoutes } from "react-router-dom";
+
+function App() {
+  const routesArray = [
+    {
+      path: "*",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/admin-home",
+      element: <AdminHome />,
+    },
+  ];
+  let routesElement = useRoutes(routesArray);
+  return (
+    <AuthProvider>
+      <div>{routesElement}</div>
+    </AuthProvider>
+  );
+}
+
+export default App;
+/*
+import React from 'react'
+import Login from './components/login/Login'
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+*/
