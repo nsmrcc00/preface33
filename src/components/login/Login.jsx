467cdefd5e8d2de8e1c84ea6b1138ec9b76e0124
nsmrcc00 <nsmrcc00@protonmail.com>
2024-04-27 23:15:37 +0800

    Initial commit

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react"
+import { Navigate, Link } from 'react-router-dom'
+import { useAuth } from "../../contexts/authContexts"
+import { doSignInWithEmailAndPassword } from "../../firebase/auth"
+
+const Login = () => {
+    const { userLoggedIn } = useAuth()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isSigningIn, setIsSigningIn] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const logIn = async (e) => {
+        e.preventDefault();
+        if(!isSigningIn){
+            setIsSigningIn(true)
+            await doSignInWithEmailAndPassword(email, password).then((userCredential) => {
+                console.log(userCredential);
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+    }
+    
+    /*
+    const logIn = async (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    <button
+                    type="submit"
+                >Log In</button>
+    */
+
+    return (
+        <div>
+            {userLoggedIn && (<Navigate to={'/admin-home'} replace={true} />)}
+            <form onSubmit={logIn}>
+                <h1>PreFace</h1>
+                <input 
+                    type="email" 
+                    placeholder='Email' 
+                    value={email}
+                    onChange={(e) => { setEmail(e.target.value) }}
+                ></input>
+
+                <input 
+                    type="password" 
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => { setPassword(e.target.value) }}
+                ></input>
+                
+                {errorMessage && (<span>{errorMessage}</span>)}
+
+
+                <button
+                    type="submit"
+                    disabled={isSigningIn}
+                    className={`${isSigningIn}`}
+                >
+                    {isSigningIn ? 'Signing In...' : 'Sign In'}
+                </button>
+
+                
+                
+
+            </form>  
+        </div>
+    )
+}
+
+export default Login
---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5ejxQ-A3BX7RdaiaAWEkgli5iuRjcz-s",
+  authDomain: "preface-b43fc.firebaseapp.com",
+  databaseURL: "https://preface-b43fc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "preface-b43fc",
+  storageBucket: "preface-b43fc.appspot.com",
+  messagingSenderId: "600568954766",
+  appId: "1:600568954766:web:ebcb36eb5047a130c1eedf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export { app, auth }
