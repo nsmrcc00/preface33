eaf343ff4c2084933311b977d99c8b825eedd119
nsmrcc00 <nsmrcc00@protonmail.com>
2024-05-07 20:18:57 +0800

    navigation ulit

---
+++ src/components/add-account/StudentAccount.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../header/Header'
+
+const StudentAccount = () => {
+  return (
+    <>
+        <Header/>
+        <div>
+            <h1>Student Account</h1>
+        </div>
+    </>
+
+  )
+}
+
+export default StudentAccount
