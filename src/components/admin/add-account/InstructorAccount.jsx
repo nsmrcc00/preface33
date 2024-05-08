39fd13090a551f3c16a3cc467aea7a54bde03433
nsmrcc00 <manahan.nes9@gmail.com>
2024-05-08 16:29:48 +0800

    ayusin pag-uwi

--- src/components/admin/add-account/InstructorAccount.jsx
+++ src/components/admin/add-account/InstructorAccount.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import Header from '../../header/Header'
-import addSubject from '../add-subject/addSubject'
+import AddSubject from '../add-subject/addSubject'
+
+
 
 const InsAccount = () => {
   return (
     <>
-    <Header/>
-    <main>
-      <section>
-        <form>
-          <input></input>
-          <input></input>
-          <input></input>
-        </form>
-        <addSubject/>
-      </section>
-    </main>
+      <Header/>
+      <AddSubject/>
     </>
-    
   )
 }
 
