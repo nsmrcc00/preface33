411b7788cf758d06898b5a57375386bcb77cf35a
nsmrcc00 <nsmrcc00@protonmail.com>
2024-06-05 23:11:19 +0800

    v0.5

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from 'react'
 import { Navigate, Link } from 'react-router-dom'
 import { useAuth } from "../../contexts/authContext"
 import { doSignInWithEmailAndPassword } from "../../firebase/auth"
