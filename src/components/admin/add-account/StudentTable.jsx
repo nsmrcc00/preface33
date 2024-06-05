eb69bb3a0de147d66af50577d28c4d498d5e8a3b
nsmrcc00 <nsmrcc00@protonmail.com>
2024-06-05 23:03:10 +0800

    can view table in asc and dec order

--- src/components/admin/add-account/StudentTable.jsx
+++ src/components/admin/add-account/StudentTable.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { collection, query, where, onSnapshot } from 'firebase/firestore';
 import { db } from '../../../firebase/firebase'; // Import your Firestore instance
+import useSortableData from '../../table-sort/TableSort';
 
 const StudentTable = ({ setSelectedAccount }) => {
     const [students, setStudents] = useState([]);
@@ -37,30 +38,39 @@ const StudentTable = ({ setSelectedAccount }) => {
                student.section && student.section.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
+    const { items: sortedStudents, requestSort, sortConfig } = useSortableData(filteredStudents);
+
+    const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+            return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
+
     return (
-        <div style={{overflowX: 'auto'}}>
-          <h2>Accounts List</h2>
+        <div style={{ overflowX: 'auto' }}>
+            <h2>Accounts List</h2>
             <input
                 type="text"
                 placeholder="Search Students..."
                 value={searchTerm}
                 onChange={handleSearchChange}
                 style={{
-                  margin: '0px 0px 10px'
+                    margin: '0px 0px 10px'
                 }}
             />
             <table className='striped-table'>
                 <thead>
                     <tr>
-                        <th>ID Number</th>
-                        <th>Email</th>
-                        <th>Name</th>
-                        <th>Section</th>
+                        <th onClick={() => requestSort('idNumber')} className={getClassNamesFor('idNumber')}>ID Number</th>
+                        <th onClick={() => requestSort('email')} className={getClassNamesFor('email')}>Email</th>
+                        <th onClick={() => requestSort('name.firstName')} className={getClassNamesFor('name.firstName')}>Name</th>
+                        <th onClick={() => requestSort('section')} className={getClassNamesFor('section')}>Section</th>
                         <th>MAC Address</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredStudents.map((student) => (
+                    {sortedStudents.map((student) => (
                         <tr key={student.id} onClick={() => setSelectedAccount(student)}>
                             <td>{student.idNumber}</td>
                             <td>{student.email}</td>
