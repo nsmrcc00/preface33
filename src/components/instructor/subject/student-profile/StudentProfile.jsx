8abb385a922ea97dc598aa8202a8e87b1d52df46
nsmrcc00 <nsmrcc00@protonmail.com>
2024-06-10 00:46:52 +0800

    student profile/report placeholder

--- src/components/instructor/subject/student-profile/StudentProfile.jsx
+++ src/components/instructor/subject/student-profile/StudentProfile.jsx
@@ -1,36 +1,52 @@
-import InstructorHeader from "../../../header/InstructorHeader"
-
+import InstructorHeader from "../../../header/InstructorHeader";
 
 const StudentProfile = () => {
   return (
     <>
       <header>
-        <InstructorHeader/>
+        <InstructorHeader />
       </header>
-      <main>
-      <div className="table-container">
-        <p> Student Name's Attendance</p>
-        <table className="striped-table">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <p>Total number of absences: </p>
-      </div> 
-      </main>      
+      <main className="subject-home">
+        <section className="subject-home-container">
+          <div className="table-container">
+            <h3>Student Name's Attendance</h3>
+            <table className="striped-table">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>a</td>
+                  <td>a</td>
+                  <td>a</td>
+                </tr>
+                <tr>
+                  <td>a</td>
+                  <td>a</td>
+                  <td>a</td>
+                </tr>
+                <tr>
+                  <td>a</td>
+                  <td>a</td>
+                  <td>a</td>
+                </tr>
+                <tr>
+                  <td>a</td>
+                  <td>a</td>
+                  <td>a</td>
+                </tr>
+              </tbody>
+            </table>
+            <p>Total number of absences: </p>
+          </div>
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default StudentProfile
+export default StudentProfile;
